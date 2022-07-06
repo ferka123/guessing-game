4bfcaeb472dd79667012d197b97150f57a129411
ferka123 <ferka911@gmail.com>
2022-07-06 12:57:20 +0300

    feat: guessing game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.pick = this.min+Math.round((this.max-this.min)/2)
     }
 
     lower() {
-
+        this.max = this.pick;
     }
 
     greater() {
-
+        this.min = this.pick;
     }
 }
 
